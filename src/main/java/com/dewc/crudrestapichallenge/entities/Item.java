10d603098e09bbb9e1c12dec6b00ce5075762f5e
Anthony Seager <32993852+mo0nmann@users.noreply.github.com>
2023-08-04 15:06:03 +0930

    fix: fix autogeneration issues

--- src/main/java/com/dewc/crudrestapichallenge/entities/Item.java
+++ src/main/java/com/dewc/crudrestapichallenge/entities/Item.java
@@ -3,6 +3,7 @@ package com.dewc.crudrestapichallenge.entities;
 import java.io.Serializable;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.Data;
@@ -13,9 +14,10 @@ import lombok.Data;
 public class Item implements Serializable {
     
     @Id
+    @GeneratedValue
     private Long id;
 
     private String name;
     private String description;
-    
+
 }
